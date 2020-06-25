a989ee96ca3032f82425ccb5b0744530f8ad85e8
Anthony Flores <anthony.flores2293@gmail.com>
2020-06-24 22:57:39 -0700

    Modified Sidebar to include individual tasks, configured helpers for tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Checkbox } from "./Checkbox";
 import { useTasks } from "../hooks";
-require("dotenv").config();
+import { collatedTasks } from "../constants";
+import { getTitle, getCollatedTitle, getCollatedTasksExist } from "../helpers";
+import { useSelectedProjectValue, useProjectsValue } from "../context";
 
 export const Tasks = () => {
-  const { tasks } = useTasks("1");
-
-  console.log(tasks);
-  console.log(process.env.REACT_APP_API_KEY);
+  const { selectedProject } = useSelectedProjectValue();
+  const { tasks } = useTasks(selectedProject);
+  const { projects } = useProjectsValue();
 
   let projectName = "";
+
+  if (projects && selectedProject && !getCollatedTasksExist(selectedProject)) {
+    projectName = getTitle(projects, selectedProject).name;
+  }
+
+  if (getCollatedTasksExist(selectedProject) && selectedProject) {
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+  }
+
+  useEffect(() => {
+    document.title = `${projectName}: Todoist`;
+  });
 
   return (
     <div className="tasks" data-testid="tasks">
